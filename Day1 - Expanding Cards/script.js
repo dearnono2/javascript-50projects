c04385337d9d0f992f42e2915df7d71bd7b5cff5
dearnono2 <dearnono2@gmail.com>
2023-03-09 00:33:03 +0900

    day1 expanding cards fin

---
+++ Day1 - Expanding Cards/script.js
@@ -0,0 +1,15 @@
+const panels = document.querySelectorAll('.panel');
+
+
+panels.forEach(el => {
+  el.addEventListener('click', (e) => {
+    removeActiveClasses()
+    el.classList.add('active');
+  })
+})
+
+function removeActiveClasses() {
+  panels.forEach(el => {
+    el.classList.remove('active')
+  })
+}
